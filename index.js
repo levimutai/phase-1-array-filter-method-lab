b69673dade9442bb36e1684af4652c9f507043ab
just ivel554 <justivel554@justs-MacBook-Pro.local>
2025-06-28 20:38:50 +0300

    Complete Array Filter Method Lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+// Case-insensitive match of full name
+function findMatching(drivers, query) {
+  return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+}
+
+// Match names that start with the provided letters
+function fuzzyMatch(drivers, query) {
+  return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+}
+
+// Match driver objects where name property matches the query
+function matchName(drivers, query) {
+  return drivers.filter(driver => driver.name === query);
+}
